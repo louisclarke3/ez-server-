3e0bc12bd90fe74403f3bffc0ab8fca4ff10d82f
louisclarke3 <louisclarke3@gmail.com>
2025-04-14 22:08:56 +1000

    Update reminders.js

--- routes/reminders.js
+++ routes/reminders.js
@@ -1,17 +1,21 @@
+const mongoose = require("mongoose");
 
-const express = require("express");
-const router = express.Router();
-const Reminder = require("../models/reminder");
+const reminderSchema = new mongoose.Schema(
+  {
+    label: {
+      type: String,
+      required: true,
+    },
+    dueDate: {
+      type: Date,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
-router.get("/", async (req, res) => {
-  const data = await Reminder.find().sort({ createdAt: -1 });
-  res.json(data);
-});
-
-router.post("/", async (req, res) => {
-  const reminder = new Reminder({ label: req.body.label });
-  await reminder.save();
-  res.json(reminder);
-});
-
-module.exports = router;
+module.exports = mongoose.model("Reminder", reminderSchema);
