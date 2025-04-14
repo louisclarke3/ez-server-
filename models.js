3447b5c74ba6968a2b3c9364be62588908774e83
louisclarke3 <louisclarke3@gmail.com>
2025-04-14 21:53:23 +1000

    Add Reminder model schema

--- models.js
+++ models.js
@@ -1,12 +1,12 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const ReminderSchema = new mongoose.Schema({
-  title: String,
+const reminderSchema = new mongoose.Schema({
+  label: String,
   dueDate: Date,
   completed: {
     type: Boolean,
     default: false
   }
-});
+}, { timestamps: true });
 
-module.exports = mongoose.model("Reminder", ReminderSchema);
+module.exports = mongoose.model('Reminder', reminderSchema);
